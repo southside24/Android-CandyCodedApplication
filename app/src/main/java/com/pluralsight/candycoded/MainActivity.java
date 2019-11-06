b28428d80f1222c5d60fe04ac0471b4b7045a34d
southside24 <opeyemiabdulsalam2442@gmail.com>
2019-11-06 23:46:50 +0100

    project completed

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -91,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
     // TODO - Task 1 - Show Store Information Activity
     // ***
 
+
     private void addCandiesToDatabase(Candy[] candies) {
         SQLiteDatabase db = candyDbHelper.getWritableDatabase();
 
